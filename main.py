a93c0c6a149ad041e7d4b24a045ecaee2113f8a1
Atharva <63628518+atharva-malik@users.noreply.github.com>
2023-03-27 21:44:32 +1100

    finished base game

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import random
 
+
+def get_offer(briefcases):
+    offer = sum(briefcases.values()) / len(briefcases) #TODO: Try to make this more exciting
+    return offer
+
+
 def deal_or_no_deal():
     briefcases= {}
+    offer = 0
     for i in range(1, 27):
         briefcases[str(i)] = random.randint(1, 1000000)
-    print(briefcases)
-    remaining_briefcases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                            16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+    remaining_briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    briefcases_to_eliminate = 6
+    print("You need to eliminate", briefcases_to_eliminate, "briefcases. What briefcases would you like to eliminate?")
+    for i in range(0, briefcases_to_eliminate):
+        number_to_eliminate = int(input("Briefcase to eliminate: "))
+        print("You removed briefcase", number_to_eliminate, "Which contained", briefcases.pop(str(number_to_eliminate)))
+        remaining_briefcases.remove(number_to_eliminate)
+    briefcases_to_eliminate -= 1
+    offer = get_offer(briefcases)
+    print("Remaining briefcases:", briefcases, "List", remaining_briefcases, "\nOffer: $", offer)
+    while briefcases_to_eliminate >= 1:
+        offer = get_offer(briefcases)
+        print("Remaining briefcases:", len(remaining_briefcases), "Offer: $", offer, "Deal or no deal?")
+        choice = input("[D]eal or [n]o deal? ")
+        if choice.lower() == "d":
+            print("Good game! You got an offer of $", offer, "and you took it! You won $", offer, "! See you next time!")
+        elif choice.lower() == "n":
+            if briefcases_to_eliminate == 1 and len(remaining_briefcases) == 1:
+                print("You have one briefcase left. You must take it.")
+                print("You won $", briefcases.pop(str(remaining_briefcases[0])), "!")
+                break
+            elif briefcases_to_eliminate == 1:
+                print("There are", len(remaining_briefcases), "briefcases left. Pick which one you will discard.")
+                number_to_eliminate = int(input("Briefcase to eliminate: "))
+                print("You removed briefcase", number_to_eliminate, "Which contained", briefcases.pop(str(number_to_eliminate)))
+                remaining_briefcases.remove(number_to_eliminate)
+            else:
+                print("There are", len(remaining_briefcases), "briefcases left. Pick", briefcases_to_eliminate, "that you will discard.")
+                for i in range(0, briefcases_to_eliminate):
+                    number_to_eliminate = int(input("Briefcase to eliminate: "))
+                    print("You removed briefcase", number_to_eliminate, "Which contained", briefcases.pop(str(number_to_eliminate)))
+                    remaining_briefcases.remove(number_to_eliminate)
+                briefcases_to_eliminate -= 1
+        else:
+            continue
 
+print("You know how to play this. Please add instructions later")
 deal_or_no_deal()
